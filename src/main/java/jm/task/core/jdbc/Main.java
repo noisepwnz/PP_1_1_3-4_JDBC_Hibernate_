26f32f54e2e499dae4b7282df357abbcd55177d3
nikolay <noisepwnz@mail.ru>
2022-08-15 12:38:43 +0500

    dopisal Main

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,19 +1,25 @@
 package jm.task.core.jdbc;
 
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
 import jm.task.core.jdbc.util.Util;
 
 import java.sql.Driver;
 
 public class Main {
     public static void main(String[] args) {
-        UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
+        UserService userService = new UserServiceImpl();
+        userService.createUsersTable();
+        userService.saveUser("Nikolay", "Belov", (byte) 23);
+        userService.saveUser("Kirill", "Razboykin", (byte) 24);
+        userService.saveUser("Roman", "Romanov", (byte) 24);
+        userService.saveUser("Andrey", "Golubev", (byte) 50);
+        userService.removeUserById(4);
+        userService.getAllUsers();
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
 
-    //    userDaoJDBC.saveUser("Masha", "Belov", (byte) 25);
-//        userDaoJDBC.saveUser("Kirill", "Razboykin", (byte) 24);
-           //userDaoJDBC.removeUserById(3);
-  System.out.println(userDaoJDBC.getAllUsers());
-       // userDaoJDBC.cleanUsersTable();
 
     }
 }
